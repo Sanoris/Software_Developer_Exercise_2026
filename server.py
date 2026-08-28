01ca6473be2911bf0aa9b66402e3b5fe00e0ed30
Matthew Hendricks <matthew91895@yahoo.com>
2026-08-28 17:22:44 -0500

    testing pandas merge

--- server.py
+++ server.py
@@ -3,10 +3,16 @@ import pandas as pd
 
 app = Flask(__name__)
 
+patient_details = pd.read_csv("./data/fake_patient_details.csv")
+patient_diagnosis = pd.read_csv("./data/fake_patient_diagnosis.csv")
+patient_genes = pd.read_csv("./data/fake_patient_genes.csv")
+
+df = pd.merge(patient_details, patient_diagnosis, on="patient_id")
+df = pd.merge(df, patient_genes, on="patient_id")
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return df.to_html()
 
 
 if __name__ == "__main__":
